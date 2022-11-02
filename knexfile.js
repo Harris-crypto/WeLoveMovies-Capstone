f234e119225bb95fd4b49bc2a5ec8d6c50549453
Harris-crypto <harrisrmeglen@gmail.com>
2022-11-02 09:51:28 -0700

    fixed seeding issue

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL="postgresql://postgres@localhost/postgres",
+  DATABASE_URL="postgres://twgqhaph:wfNSVIpJt1PWgjT1Hq9zk0n5I5TFakK_@heffalump.db.elephantsql.com/twgqhaph",
 } = process.env;
 
 module.exports = {
